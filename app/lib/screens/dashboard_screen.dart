ed80c9214870b70745426daf1be3b34a510e4ea9
firattok1987 <firattok1987@gmail.com>
2026-02-13 13:27:20 +0300

    Create dashboard_screen.dart

---
+++ app/lib/screens/dashboard_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Dashboard",
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+    );
+  }
+}
